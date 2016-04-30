1d8574aad487a4e97c8ecbbe2bef273aef353f78
Dkra <dkra4741@gmail.com>
2016-04-30 17:10:20 +0800

    about to add youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new Component - produce html
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// A Component Class
+// const App = function() {
+//   return <div>123</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>Hi!</div>
+}
+
+
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+
+// should pass a instance
+// App is a class
+// <App></App> is a instances & is equal to </App>
+ReactDOM.render(<App />, document.querySelector('.container'));
